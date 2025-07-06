89649d505558261b894e99027253aa4eb4134842
Agougil Mohamed <agougilmohamed03@gmail.com>
2025-07-06 14:36:10 +0100

    init nativewind and bottom navigation screens

---
+++ app/(tabs)/decks.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Decks = () => {
+  return (
+    <View>
+      <Text>Welcome to the decks Screen!</Text>
+    </View>
+  );
+};
+
+export default Decks;
---
+++ app/(tabs)/progress.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Progress = () => {
+  return (
+    <View>
+      <Text>Welcome to the Progress Screen!</Text>
+    </View>
+  );
+};
+
+export default Progress;
